76629eab80f6a0d80ac18eda6074e8ff38383b70
Ali Siddiqui <alisiddiquia22@yahoo.com>
2023-05-12 10:43:56 -0500

    updated and finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 // Write your DOM code here!
+let body = document.querySelector(`body`)
+
+let red = document.querySelector('.red')
+red.addEventListener('click', () => {
+    body.style.backgroundColor ='red'
+})
+
+let white = document.querySelector('.white')
+white.addEventListener('click', () => {
+    body.style.backgroundColor ='white'
+})
+
+let blue = document.querySelector('.blue')
+blue.addEventListener('click', () => {
+    body.style.backgroundColor ='blue'
+})
+
+let yellow = document.querySelector('.yellow')
+yellow.addEventListener('click', () => {
+    body.style.backgroundColor ='yellow'
+})
+
